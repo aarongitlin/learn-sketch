7d8ed5f51512a9641bc93b090a4e7bb922e9f72d
Aaron Gitlin <aarongitlin@gmail.com>
2015-02-10 09:45:57 -0500

    added keynote section, still barebones

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -1,1 +1,17 @@
 //= require 'vendor/jquery-1.10.2.min'
+
+$( document ).ready(function() {
+
+if ( $("body").hasClass('sketch') ) {
+
+  $(".header-nav .sketch-link").addClass('activated');
+
+}
+
+if ( $("body").hasClass('keynote') ) {
+
+  $(".header-nav .keynote-link").addClass('activated');
+
+}
+
+});
